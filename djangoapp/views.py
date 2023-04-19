ffcd2f28640fc7236c8ddfbe3996b10caf143a2c
trls888s <trls888s@ASUSTUF.io>
2023-04-19 12:15:07 +1200

    modified:   djangoapp/admin.py 	modified:   djangoapp/models.py 	modified:   djangoapp/restapis.py 	modified:   djangoapp/templates/djangoapp/about.html 	modified:   djangoapp/templates/djangoapp/add_review.html 	modified:   djangoapp/templates/djangoapp/contact.html 	modified:   djangoapp/templates/djangoapp/dealer_details.html 	modified:   djangoapp/templates/djangoapp/index.html 	modified:   djangoapp/templates/djangoapp/registration.html 	new file:   djangoapp/templates/footer.html 	modified:   djangoapp/urls.py 	modified:   djangoapp/views.py

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -95,18 +95,83 @@ def registration_request(request):
         else:
             return render(request, 'djangoapp/registration.html', context)
 
+
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
+        context = {}
+        url = "CloudantPath"
+        # Get dealers from the URL
+        context["dealerships"] = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        #dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
         return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
+def get_dealer_details(request, dealer_id):
+    context = {}
+    if request.method == "GET":
+        url = 'Cloudant Path'
+        reviews = get_dealer_reviews_from_cf(url,dealer_id)
+        context = {
+            "reviews":  reviews, 
+            "dealer_id": dealer_id
+        }
+
+        return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+def add_review(request, dealer_id):
+    # User must be logged in before posting a review
+    if request.user.is_authenticated:
+        # GET request renders the page with the form for filling out a review
+        if request.method == "GET":
+            url = "Cloundant_Path"
+            # Get dealer details from the API
+            context = {
+                "cars": CarModel.objects.all(),
+                "dealer": get_dealer_by_id(url, dealer_id=dealer_id),
+            }
+            return render(request, 'djangoapp/add_review.html', context)
+
+        # POST request posts the content in the review submission form to the Cloudant DB using the post_review Cloud Function
+        if request.method == "POST":
+            form = request.POST
+            review = dict()
+            review["name"] = "{request.user.first_name} {request.user.last_name}"
+            review["dealership"] = dealer_id
+            review["review"] = form["content"]
+            review["purchase"] = form.get("purchasecheck")
+            if review["purchase"]:
+                review["purchase_date"] = datetime.strptime(form.get("purchasedate"), "%m/%d/%Y").isoformat()
+            car = CarModel.objects.get(pk=form["car"])
+            review["car_make"] = car.car_make.name
+            review["car_model"] = car.name
+            review["car_year"] = car.year
+            
+            # If the user bought the car, get the purchase date
+            if form.get("purchasecheck"):
+                review["purchase_date"] = datetime.strptime(form.get("purchasedate"), "%m/%d/%Y").isoformat()
+            else: 
+                review["purchase_date"] = None
+
+            url = "FIX_Cloudant_API_Path"  
+            json_payload = {"review": review}  
+            result = post_request(url, json_payload, dealerId=dealer_id)
+            if int(result.status_code) == 200:
+                print("Review posted successfully.")
+
+            # After posting the review the user is redirected back to the dealer details page
+            return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
+
+    else:
+        # If user isn't logged in, redirect to login page
+        print("User must be authenticated before posting a review. Please log in.")
+        return redirect("/djangoapp/login")
+
+
 
